1989d554add7105119d5b3ea4cf86d24c15ea1b5
azkawm <willianazka427@gmail.com>
2025-03-17 15:04:06 +0700

    logged to node Js

---
+++ nodeJs/index.js
@@ -0,0 +1,32 @@
+import { http, createPublicClient, parseAbiItem } from "viem";
+import { base } from "viem/chains";
+
+const client = createPublicClient({
+  chain: base,
+  transport: http(
+    "https://base-sepolia.infura.io/v3/499f4f21a89e426a8c9da41ecd7fea97"
+  ),
+});
+
+const event = parseAbiItem(
+  "event Transfer(address sender, address wallet, uint256 amount)"
+);
+
+const address = "0xd8Bf86775A08145429519513b0cA53b963e4600A";
+
+// Get initial event logs (last 20 blocks)
+const blockNumber = await client.getBlockNumber();
+const logs = await client.getLogs({
+  event,
+  fromBlock: blockNumber - 20n,
+  toBlock: blockNumber,
+});
+
+// Watch for new event logs
+client.watchEvent({
+  address,
+  event,
+  onLogs: (logs) => {
+    console.log(logs[0]);
+  },
+});
