4308003e0c9af88dcbc07dbf1a5d4ce106511e91
azkawm <willianazka427@gmail.com>
2025-03-18 12:45:51 +0700

    add node modules to gitignore

--- nodeJs/index.js
+++ nodeJs/index.js
@@ -12,12 +12,15 @@ const event = parseAbiItem(
   "event Transfer(address sender, address wallet, uint256 amount)"
 );
 
+const error = parseAbiItem("error AmountShouldNotBeZero()");
+
 const address = "0xd8Bf86775A08145429519513b0cA53b963e4600A";
 
 // Get initial event logs (last 20 blocks)
 const blockNumber = await client.getBlockNumber();
 const logs = await client.getLogs({
   event,
+  error,
   fromBlock: blockNumber - 20n,
   toBlock: blockNumber,
 });
@@ -27,6 +30,13 @@ client.watchEvent({
   address,
   event,
   onLogs: (logs) => {
-    console.log(logs[0]);
+    try {
+      console.log(logs[0]);
+    } catch (error) {
+      console.error(
+        "Transaction reverted:",
+        error.shortMessage || error.message
+      );
+    }
   },
 });
